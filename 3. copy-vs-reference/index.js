9088af86da39816773611ca5b5171526b92cfbf5
prasanth <chaduvulaprasanth1998@gmail.com>
2019-08-13 14:01:03 +0530

    completed index.js

--- 3. copy-vs-reference/index.js
+++ 3. copy-vs-reference/index.js
@@ -4,7 +4,7 @@ var x = 10;
 var y = "abc";
 var a = x;
 var b = y;
-
+---------------- x=10, y="abc", a=10, b="abc" --------------------
 // Value of x, y, a, b
 var x = 10;
 var y = "abc";
@@ -13,19 +13,30 @@ var b = y;
 a = 5;
 b = "def";
 
+---------------- x=10, y="abc", a=5, b="def" --------------------
+
 // Value of arr & arrCopy
 var arr = [1];
 var arrCopy = arr;
 arr.push(2);
 
+---------------- arr     arrCopy      --------------------
+                0: 1     0: 1
+                1: 2     1: 2 
+
+
 // Value of obj
 var obj = { name: "ryan" };
 obj = { surname: "florance" };
+---------------- {surname: "florance"} ----------------
+----------------  surname: "florance"  ----------------
 
 // What's the output.
 var arr = ["Hi"];
 var arr2 = arr;
 console.log(arr === arr2);
+//output
+----------------  true  ----------------  
 
 // What's the output.
 var arr1 = ["Hi!"];
@@ -33,9 +44,12 @@ var arr2 = ["Hi!"];
 console.log(arr1 === arr2);
 console.log(arr == arr2);
 
+---------------- false ---------------- 
 //What's the output
 console.log([10] === [10]);
 
+
+----------------  false  ---------------- 
 // What's the output?
 
 function personDetails(person) {
@@ -52,8 +66,10 @@ var personObj1 = {
   age: 30
 };
 var personObj2 = personDetails(personObj1);
-console.log(personObj1); // -> ?
-console.log(personObj2); // -> ?
+console.log(personObj1); // -> ?{name: "Alex", age: 25}
+console.log(personObj2); // -> ?{name: "Alex", age: 25}
+
+ 
 
 // Guess the output
 var oldArray = [];
@@ -62,12 +78,19 @@ object.newArray = oldArray;
 oldArray.push(10);
 console.log(object.newArray === oldArray);
 
+---------------- true  ---------------
+
 // Guess the output
 var a = 5;
 var b = a;
 a = 10;
 console.log(a);
 console.log(b);
+
+---------------- 10  ---------------
+                  5
+
+
 
 // What's the output?
 var a = {};
@@ -76,6 +99,11 @@ a.a = 1;
 console.log(a);
 console.log(b);
 
+---------------- {a: 1}  ---------------
+                 {a: 1}
+
+
+
 // What's the output.
 var a = [];
 var b = a;
@@ -83,6 +111,11 @@ a.push(1);
 console.log(a); // [1]
 console.log(b); // [1]
 console.log(a === b);
+
+
+--------------- true ---------------
+
+
 
 // Clone the object person in clone
 var person = {
@@ -95,3 +128,6 @@ var person = {
 };
 
 var clone = {};
+
+
+---------------clone=person  ---------------
